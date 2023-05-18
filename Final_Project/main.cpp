a68cfe059c1e563c117c0929ffecb92d7efef031
yungsheng0825 <a0989122811@gmail.com>
2023-05-18 16:59:24 +0800

    week14

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <GL/glut.h>
 #include "glm.h"
-GLMmodel * head = NULL;
-GLMmodel * body = NULL;
-GLMmodel * lhand = NULL;
-GLMmodel * rhand = NULL;
-int show[4]={1,0,0,0};
 
-
+GLMmodel * head =NULL;
+GLMmodel * body =NULL;
+GLMmodel * Ruparm =NULL;
+GLMmodel * Rhand =NULL;
+int show[4] ={1,1,1,1};
+int ID =2;
 float teapotX=0,teapotY=0;
+float angle=0;
 FILE * fout =NULL;
 FILE * fin =NULL;
-void keyboard(unsigned char key ,int x,int y){
-    if(key=='0') show[0] = ! show[0];
-    if(key=='1') show[1] = ! show[1];
-    if(key=='2') show[2] = ! show[2];
-    if(key=='3') show[3] = ! show[3];
+void keyboard(unsigned char key,int x,int y)
+{
+    if(key=='0') ID = 0;
+    if(key=='1') ID = 1;
+    if(key=='2') ID = 2;
+    if(key=='3') ID = 3;
     glutPostRedisplay();
 }
 void display()
@@ -24,42 +26,71 @@ void display()
     if(head==NULL){
         head = glmReadOBJ("model/head.obj");
         body = glmReadOBJ("model/body.obj");
-        lhand = glmReadOBJ("model/lhand.obj");
-        rhand = glmReadOBJ("model/rhand.obj");
+        Ruparm = glmReadOBJ("model/Ruparm.obj");
+        Rhand = glmReadOBJ("model/Rhand.obj");
     }
     glPushMatrix();
-        glScaled(0.3,0.3,0.3);
-    glPushMatrix();
-      glTranslatef(teapotX,teapotY,0);
-      if(show[0])  glmDraw(head, GLM_MATERIAL);
+        glScalef(0.3,0.3,0.3);
+        glPushMatrix();
+        //glTranslatef(teapotX,teapotY,0);
+            if(ID==0) glColor3f(1,0,0);
+            else glColor3f(1,1,1);
+            if(show[0]) glmDraw(head,GLM_MATERIAL);
+
+        glPopMatrix();
+
+        if(ID==1) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
+        if(show[1])glmDraw(body,GLM_MATERIAL);
+
+        glPushMatrix();
+            glTranslatef(-1.360000,+0.360000,0);
+            glRotatef(angle,0,0,1);
+            glTranslatef(1.360000,-0.360000,0);
+            if(ID==2) glColor3f(1,0,0);
+            else glColor3f(1,1,1);
+            if(show[2])glmDraw(Ruparm,GLM_MATERIAL);
+            glPushMatrix();
+                glTranslatef(-1.959999,+0.080000,0);
+                glRotatef(angle,0,0,1);
+                glTranslatef(1.959999,-0.080000,0);
+
+                if(ID==3) glColor3f(1,0,0);
+                else glColor3f(1,1,1);
+                if(show[3])glmDraw(Rhand,GLM_MATERIAL);
+            glPopMatrix();
+        glPopMatrix();
+
     glPopMatrix();
-      if(show[1])  glmDraw(body, GLM_MATERIAL);
-      if(show[2]) glmDraw(lhand, GLM_MATERIAL);
-      if(show[3]) glmDraw(rhand, GLM_MATERIAL);
-    glPopMatrix();
+    glColor3f(0,1,0);
+    glutSolidTeapot(0.02);
     glutSwapBuffers();
 }
 int oldX=0,oldY=0;
 void  mouse(int button,int state,int x,int y)
 {
-   if(state==GLUT_DOWN){
-    oldX=x;
-    oldY=y;
-   }
+
+    if(state==GLUT_DOWN){
+       oldX = x;
+       oldY = y;
+    }
 }
 void motion(int x,int y)
 {
-     teapotX=(x-150)/150.0;
-     teapotY=(150-y)/150.0;
-     oldX=x;
-     oldY=y;
-     glutPostRedisplay();
+      teapotX +=(x-oldX)/150.0*3;
+    teapotY -=(y-oldY)/150.0*3;
+    printf("glTranslate(%f,%f,0);\n",teapotX,teapotY);
+    angle += x-oldX;
+    oldX = x;
+    oldY = y;
+    glutPostRedisplay();
 }
+
 int main(int argc,char** argv)
 {
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
-    glutCreateWindow("week13");
+    glutCreateWindow("week14");
 
     glutMotionFunc(motion);
     glutDisplayFunc(display);

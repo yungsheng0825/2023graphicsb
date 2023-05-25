fc86c6eec180a882b78a7913b18ae8f62431fbec
yungsheng0825 <a0989122811@gmail.com>
2023-05-25 16:41:43 +0800

    week15

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -9,7 +9,8 @@ GLMmodel * Rhand =NULL;
 int show[4] ={1,1,1,1};
 int ID =2;
 float teapotX=0,teapotY=0;
-float angle=0;
+float angle[20]={};
+///float angle=0;
 FILE * fout =NULL;
 FILE * fin =NULL;
 void keyboard(unsigned char key,int x,int y)
@@ -45,14 +46,14 @@ void display()
 
         glPushMatrix();
             glTranslatef(-1.360000,+0.360000,0);
-            glRotatef(angle,0,0,1);
+            glRotatef(angle[2],0,0,1);
             glTranslatef(1.360000,-0.360000,0);
             if(ID==2) glColor3f(1,0,0);
             else glColor3f(1,1,1);
             if(show[2])glmDraw(Ruparm,GLM_MATERIAL);
             glPushMatrix();
                 glTranslatef(-1.959999,+0.080000,0);
-                glRotatef(angle,0,0,1);
+                glRotatef(angle[3],0,0,1);
                 glTranslatef(1.959999,-0.080000,0);
 
                 if(ID==3) glColor3f(1,0,0);
@@ -80,7 +81,7 @@ void motion(int x,int y)
       teapotX +=(x-oldX)/150.0*3;
     teapotY -=(y-oldY)/150.0*3;
     printf("glTranslate(%f,%f,0);\n",teapotX,teapotY);
-    angle += x-oldX;
+    angle[ID] += x-oldX;
     oldX = x;
     oldY = y;
     glutPostRedisplay();
